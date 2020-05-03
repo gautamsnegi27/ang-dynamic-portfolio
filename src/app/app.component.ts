cf3f6de46c3a4c4b19ad5a759337027916bf2c8b
gautamnegi <gautam.negi@coviam.com>
2020-05-03 05:45:54 +0530

    basic working done till HTML version.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ang-dynamic-portfolio';
+
+  ngOnInit() {
+    AOS.init({
+      easing: 'ease',
+      duration: 3000,
+      once: true
+    });
+
+    $(document).ready(function () {
+      $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+      });
+
+      $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+      });
+
+      $('nav a[href*="#"]').on('click', function () {
+        $('html,body').animate({
+          scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 500);
+      });
+
+      $('#up').on('click', function () {
+        $('html,body').animate({
+          scrollTop: 0
+        }, 500);
+      });
+
+    })
+  }
 }
