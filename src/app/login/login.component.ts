ed53fedd1cfd2c9b49ca87da7ba3a46686065e72
gautam.negi@coviam.com <>
2020-06-14 01:25:26 +0530

    mail details added.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -44,6 +44,7 @@ export class LoginComponent implements OnInit {
   emailPresent: boolean = false;
   passPattern: boolean;
   accountCreated: boolean;
+  mailbody="Please fill the below details:%0D%0A%0D%0Adescription:%0D%0A%0D%0Adesignation:%0D%0A%0D%0ASocialMediaDetails%0D%0AfbUrl:%0D%0AinstaUrl:%0D%0AlinkedinUrl:%0D%0A%0D%0Aservices%0D%0A1.%0D%0AserviceName:%0D%0AserviceDescription:%0D%0A2.%0D%0AserviceName:%0D%0AserviceDescription:%0D%0A3.%0D%0A.%0D%0A.%0D%0A.%0D%0A%0D%0A%0D%0AProjectModel%0D%0A1.%0D%0AprojectName:%0D%0AprojectDescription:%0D%0AprojectImage:%0D%0AprojectLink:%0D%0A2. %0D%0AprojectName:%0D%0AprojectDescription:%0D%0AprojectImage:%0D%0AprojectLink:%0D%0A3. %0D%0A.%0D%0A.%0D%0A.%0D%0A%0D%0AExperienceDetailModel%0D%0A%0D%0A1. %0D%0A	from:%0D%0A	to:%0D%0A	companyName:%0D%0A	jobDescription:%0D%0A2. %0D%0A	from:%0D%0A	to:%0D%0A	companyName:%0D%0A	jobDescription:%0D%0A3. %0D%0A.%0D%0A.%0D%0A.%0D%0A"
   // variable declaration ends
 
   constructor(private router: Router,
